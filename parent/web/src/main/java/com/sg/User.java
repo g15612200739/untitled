a6c65a601c79a71b626f122fc311f86390564cdb
Gao <wohub123@163.com>
2021-01-09 15:59:09 +0800

    这次时实验自己的仓库

---
+++ parent/web/src/main/java/com/sg/User.java
@@ -0,0 +1,20 @@
+package com.sg;
+
+public class User {
+    private String a;
+
+    public User() {
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public User(String a) {
+        this.a = a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+}
---
+++ parent/web/src/main/java/com/sg/test.java
@@ -0,0 +1,10 @@
+package com.sg;
+
+public class test {
+    public static void main(String[] args) {
+        int a = 123;
+        int b = a;
+
+        System.out.println(b);
+    }
+}
